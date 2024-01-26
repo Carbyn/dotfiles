2ab3b46aafaf91cfcd6370d1b4eab49981962797
Carbyn <carbyn@live.com>
2024-01-26 12:19:16 +0800

    Add pyright conf

--- config/nvim/lua/custom/configs/lspconfig.lua
+++ config/nvim/lua/custom/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { }
+local servers = { "pyright" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
@@ -12,3 +12,14 @@ for _, lsp in ipairs(servers) do
     capabilities = capabilities,
   }
 end
+
+lspconfig.pyright.setup {
+  settings = {
+    python = {
+      analysis = {
+        diagnosticMode = "off",
+        typeCheckingMode = "off",
+      },
+    },
+  },
+}
