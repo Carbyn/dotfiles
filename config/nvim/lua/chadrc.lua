8782650892d12a7c255ce1175d5658cdf4f09b15
Carbyn <carbyn@live.com>
2024-06-29 15:10:29 +0800

    Upgrade NvChad2.5

---
+++ config/nvim/lua/chadrc.lua
@@ -0,0 +1,25 @@
+-- This file needs to have same structure as nvconfig.lua 
+-- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
+
+---@type ChadrcConfig
+local M = {}
+
+M.ui = {
+	theme = "onedark",
+  transparency = true,
+
+  changed_themes = {
+    onedark = {
+      polish_hl = {
+        ["@constant"] = { fg = "#fda65f" },
+      },
+    },
+  },
+
+	-- hl_override = {
+	-- 	Comment = { italic = true },
+	-- 	["@comment"] = { italic = true },
+	-- },
+}
+
+return M
--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -1,3 +1,10 @@
+require "nvchad.options"
+
+-- add yours here!
+
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
+
 local opt = vim.opt
 local g = vim.g
 
