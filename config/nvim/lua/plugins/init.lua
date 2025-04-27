897bce4ce51952ef6b35327cf0d1ae532ed95dac
Carbyn <carbyn@live.com>
2025-04-27 11:26:52 +0800

    Add todo-comments.nvim

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -38,4 +38,13 @@ return {
 			},
 		},
 	},
+
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("todo-comments").setup()
+		end,
+		event = "BufReadPost",
+	},
 }
