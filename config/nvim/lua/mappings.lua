d762f08e04b83a9393cbe314ea88fda9d46460fe
Carbyn <carbyn@live.com>
2025-05-09 13:58:42 +0800

    Support toggle bg transparency

--- config/nvim/lua/mappings.lua
+++ config/nvim/lua/mappings.lua
@@ -8,3 +8,10 @@ map("i", "jk", "<ESC>")
 map("n", "<leader>i", function()
   require("nvchad.term").new { pos = "float" }
 end, { desc = "terminal new float term" })
+
+map(
+  "n",
+  "<leader>tt",
+  ":lua require('base46').toggle_transparency()<CR>",
+  { noremap = true, silent = true, desc = "toggle background transparency" }
+)
