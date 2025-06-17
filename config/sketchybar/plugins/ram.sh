627428f62a851914e80f58f920350fe401055ede
Carbyn <carbyn@live.com>
2025-06-17 14:43:42 +0800

    Bar: fix ram usage

--- config/sketchybar/plugins/ram.sh
+++ config/sketchybar/plugins/ram.sh
@@ -9,17 +9,11 @@ get_pages() {
 
 active=$(get_pages "Pages active")
 wired=$(get_pages "Pages wired down")
-compressed=$(get_pages "Pages occupied by compressor")
 
-# btop: used = active + wired + compressed
-used_pages=$((active + wired + compressed))
+used_pages=$((active + wired))
 used_mem=$((used_pages * pagesize))
 total_mem=$(sysctl -n hw.memsize)
 
 usage=$(awk "BEGIN {printf \"%.0f\", 100 * $used_mem / $total_mem}")
-
-# echo "Used (btop-style): $(($used_mem / 1024 / 1024)) MB"
-# echo "Total: $(($total_mem / 1024 / 1024)) MB"
-# echo "Usage: $usage %"
 
 sketchybar --set $NAME label="${usage}%"
